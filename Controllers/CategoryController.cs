2aa5986b74c4eb0101c79297b88ac4b3b9fd6449
Rafael Ferreira <osmarditto@gmail.com>
2022-03-18 00:32:44 -0300

    Initial commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Blog.Data;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Controllers
+{
+  [ApiController]
+  [Route("v1/Category")]
+  public class CategoryController : ControllerBase
+  {
+    [HttpGet]
+
+    public async Task<IActionResult> GetAllAsync([FromServices] BlogDataContext context)
+      => Ok(await context.Categories!.ToListAsync());
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context, [FromRoute] int id)
+    {
+      var category = await context.Categories!.FirstOrDefaultAsync(x => x.Id == id);
+      if (category == null) return NotFound();
+      return Ok(category);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> NewCategory([FromServices] BlogDataContext context, [FromBody] Category category)
+    {
+      await context.Categories!.AddAsync(category);
+      await context.SaveChangesAsync();
+      return Created($"/Category/{category.Id}", category);
+    }
+  }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers 
+{
+  [ApiController]
+  [Route("v1")]
+  public class HomeController : ControllerBase
+  {
+    [HttpGet("status")]
+    public IActionResult HealthCheck()
+    {
+      return Ok("Status 200: API running...");
+    }
+
+  }
+}
